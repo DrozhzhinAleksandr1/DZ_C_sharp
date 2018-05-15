79be481e02bc33299e5d79e6983c08cfa007ce6b
DrozhzhinAleksandr1 <DrozhzhinAleksandr1@gmail.com>
2018-05-15 19:26:38 +0300

    add clock 2

---
+++ DZ_Clock2/Demo.GrContainer/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Demo.GrContainer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.myClock2 = new Demo.GrContainer.MyClock();
+            this.myClock1 = new Demo.GrContainer.MyClock();
+            this.SuspendLayout();
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Enabled = true;
+            this.clockTimer.Interval = 1000;
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // myClock2
+            // 
+            this.myClock2.BackColor = System.Drawing.SystemColors.Control;
+            this.myClock2.Location = new System.Drawing.Point(228, -34);
+            this.myClock2.Name = "myClock2";
+            this.myClock2.Size = new System.Drawing.Size(472, 350);
+            this.myClock2.TabIndex = 1;
+            // 
+            // myClock1
+            // 
+            this.myClock1.BackColor = System.Drawing.SystemColors.Control;
+            this.myClock1.Location = new System.Drawing.Point(-76, -34);
+            this.myClock1.Name = "myClock1";
+            this.myClock1.Size = new System.Drawing.Size(392, 259);
+            this.myClock1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.myClock2);
+            this.Controls.Add(this.myClock1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer clockTimer;
+        private MyClock myClock1;
+        private MyClock myClock2;
+    }
+}
+
---
+++ DZ_Clock2/Demo.GrContainer/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo.GrContainer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            int baseRadius = ClientSize.Width > ClientSize.Height ? ClientSize.Height * 3 / 10 : ClientSize.Width * 3 / 10;
+
+            Graphics gr = e.Graphics;
+
+            DrawHearth(gr);
+
+            DrawArrow(Color.DeepSkyBlue, 1, gr, baseRadius-30, DateTime.Now.Second * 6);
+            DrawArrow(Color.Blue, 3,  gr, baseRadius - 30, DateTime.Now.Minute * 6);
+            DrawArrow(Color.Red, 4, gr, baseRadius - 50, DateTime.Now.Hour * 30);
+
+            DrawScale(Color.DarkViolet, 1, gr, baseRadius - 30, 10 );
+
+        }
+
+        private void DrawScale(Color color, int penWidth, Graphics gr, int radius, int length)
+        {
+            for (int i = 0; i < 60; ++i)
+            {
+                GraphicsContainer container = gr.BeginContainer(
+                    new Rectangle(ClientSize.Width / 2, ClientSize.Height * 3 / 5, ClientSize.Width, ClientSize.Height),
+                    new Rectangle(0, 0, ClientSize.Width, ClientSize.Height),
+                    GraphicsUnit.Pixel
+                );
+
+                gr.RotateTransform(i*6);
+
+                gr.DrawLine(
+                    new Pen(color, i % 5 == 0 ? penWidth + penWidth : penWidth),
+                    new Point(0, i % 5 == 0 ? -radius + length + length : -radius + length),
+                    new Point(0, -radius)
+                );
+                
+                if(i % 5 == 0)
+                {
+                    int number = i / 5;
+                    if (i == 0)
+                    {
+                        number = 12;
+                    } 
+                    gr.RotateTransform(180);
+                    SolidBrush newBrush = new SolidBrush(Color.Green);
+                    
+                    gr.DrawString(
+                        "" + number, 
+                        this.Font, 
+                        newBrush, 
+                        0 - gr.MeasureString(""+ number,this.Font).Width / 2, 
+                        radius + gr.MeasureString("" + number, this.Font).Height/5
+                    );
+                }
+                
+                gr.EndContainer(container);
+            }
+        }
+
+        private void DrawArrow(Color color, int penWidth, Graphics gr, int length, int angle)
+        {
+            GraphicsContainer container = gr.BeginContainer(
+                new Rectangle(ClientSize.Width / 2, ClientSize.Height * 3 / 5, ClientSize.Width, ClientSize.Height),
+                new Rectangle(0, 0, ClientSize.Width, ClientSize.Height),
+                GraphicsUnit.Pixel
+            );
+
+            gr.RotateTransform(angle);
+
+            gr.DrawLine(new Pen(color, penWidth),
+                new Point(0, 0),
+                new Point(0, - length)
+            );
+            gr.EndContainer(container);
+        }       
+
+        private void DrawHearth(Graphics gr)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddBezier(
+                new Point(ClientSize.Width / 2, ClientSize.Height * 2 / 7),
+                new Point(ClientSize.Width / 3, 0),
+                new Point(0, ClientSize.Height * 3 / 5),
+                new Point(ClientSize.Width / 2, ClientSize.Height - 10)
+                );
+
+            gp.AddBezier(
+               new Point(ClientSize.Width / 2, ClientSize.Height - 10),
+               new Point(ClientSize.Width, ClientSize.Height * 3 / 5),
+               new Point(ClientSize.Width * 2 / 3, 0),
+               new Point(ClientSize.Width / 2, ClientSize.Height * 2 / 7)
+               );
+            PathGradientBrush br = new PathGradientBrush(gp);
+            br.CenterPoint = new PointF(ClientSize.Width / 2, ClientSize.Height / 2);
+            br.CenterColor = Color.White;
+            br.SurroundColors = new Color[]
+            {
+                Color.Pink
+            };
+            gr.FillPath(br, gp);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
